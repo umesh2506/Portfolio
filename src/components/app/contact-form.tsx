25a96f4d8199ec679d62f3e620778bac1e1a46c0
Umesh <pagadojuumesh870@gmail.com>
2025-09-29 10:44:40 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/app/contact-form.tsx
+++ src/components/app/contact-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useActionState, useFormStatus } from 'react';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { useEffect, useRef } from 'react';
 import { useToast } from '@/hooks/use-toast';
 import { sendContactMessage } from '@/app/actions';
