f29fc9ba422ef74504f4d871c0d9dbdad0fe6c94
Umesh <pagadojuumesh870@gmail.com>
2025-09-29 09:18:13 +0000

    Interactive Resume change as Resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,7 +4,7 @@ import { ResumeEditor } from "@/components/app/resume-editor";
 export default function ResumePage() {
   return (
     <div className="flex flex-col min-h-svh">
-      <PageHeader title="Interactive Resume" />
+      <PageHeader title="Resume" />
       <main className="flex-1">
         <ResumeEditor />
       </main>
